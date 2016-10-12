a0d09df15c28d0cb694dbed41123addd9d81163f
Malcolm McLean <malcolm.mclean5@btinternet.com>
2016-10-12 17:57:24 +0100

    artifact on right bug out

--- src/resize.c
+++ src/resize.c
@@ -8,7 +8,7 @@
   resize an image using the averaging method.
   Note that dwidth and dheight must be smaller than or equal to swidth, sheight.
 
- */
+*/
 void sprshrink(unsigned char *dest, int dwidth, int dheight, unsigned char *src, int swidth, int sheight)
 {
   int x, y;
@@ -23,100 +23,108 @@ void sprshrink(unsigned char *dest, int dwidth, int dheight, unsigned char *src,
   dy = ((float)sheight)/dheight;
 
   for(yt= 0, y=0;y<dheight;y++, yt += dy)
-  {
-    yfrag = (float) ceil(yt) - yt;
-    if(yfrag == 0)
-      yfrag = 1;
-    yfrag2 = yt+dy - (float) floor(yt + dy);
-    if(yfrag2 == 0 && dy != 1.0f)
-      yfrag2 = 1;
-    
-    for(xt = 0, x=0;x<dwidth;x++, xt+= dx)
     {
-      xi = (int) xt;
-      yi = (int) yt;
-      xfrag = (float) ceil(xt) - xt;
-      if(xfrag == 0)
-        xfrag = 1;
-      xfrag2 = xt+dx - (float) floor(xt+dx);
-      if(xfrag2 == 0 && dx != 1.0f)
-        xfrag2 = 1;
-      red = xfrag * yfrag * src[(yi*swidth+xi)*4];
-      green =  xfrag * yfrag * src[(yi*swidth+xi)*4+1];
-      blue =   xfrag * yfrag * src[(yi*swidth+xi)*4+2];
-      alpha =  xfrag * yfrag * src[(yi*swidth+xi)*4+3];
-      
-      for(i=0; xi + i + 1 < xt+dx-1; i++)
-      {
-        red += yfrag * src[(yi*swidth+xi+i+1)*4];
-        green += yfrag * src[(yi*swidth+xi+i+1)*4+1];
-        blue += yfrag * src[(yi*swidth+xi+i+1)*4+2];
-        alpha += yfrag * src[(yi*swidth+xi+i+1)*4+3];
-      } 
-      
-      red += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4];
-      green += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4+1];
-      blue += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4+2];
-      alpha += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4+3];
-      for(i=0; yi+i+1 < yt +dy-1 && yi + i+1 < sheight;i++)
-      {
-        red += xfrag * src[((yi+i+1)*swidth+xi)*4];
-        green += xfrag * src[((yi+i+1)*swidth+xi)*4+1];
-        blue += xfrag * src[((yi+i+1)*swidth+xi)*4+2];
-        alpha += xfrag * src[((yi+i+1)*swidth+xi)*4+3];
-        
-		for (ii = 0; xi + ii + 1 < xt + dx - 1 && xi + ii + 1 < swidth; ii++)
+      yfrag = ceil(yt) - yt;
+      if(yfrag == 0)
+	yfrag = 1;
+      yfrag2 = yt+dy - (float) floor(yt + dy);
+      if(yfrag2 == 0 && dy != 1.0f)
+	yfrag2 = 1;
+    
+      for(xt = 0, x=0;x<dwidth;x++, xt+= dx)
 	{
-          red += src[((yi+i+1)*swidth+xi+ii+1)*4];
-          green += src[((yi+i+1)*swidth+xi+ii+1)*4+1];
-          blue += src[((yi+i+1)*swidth+xi+ii+1)*4+2];
-          alpha += src[((yi+i+1)*swidth+xi+ii+1)*4+3];
-	}
+	  xi = (int) xt;
+	  yi = (int) yt;
+	  xfrag = (float) ceil(xt) - xt;
+	  if(xfrag == 0)
+	    xfrag = 1;
+	  xfrag2 = xt+dx - (float) floor(xt+dx);
+	  if(xfrag2 == 0 && dx != 1.0f)
+	    xfrag2 = 1;
+	  red = xfrag * yfrag * src[(yi*swidth+xi)*4];
+	  green =  xfrag * yfrag * src[(yi*swidth+xi)*4+1];
+	  blue =   xfrag * yfrag * src[(yi*swidth+xi)*4+2];
+	  alpha =  xfrag * yfrag * src[(yi*swidth+xi)*4+3];
+      
+	  for(i=0; xi + i + 1 < xt+dx-1; i++)
+	    {
+	      red += yfrag * src[(yi*swidth+xi+i+1)*4];
+	      green += yfrag * src[(yi*swidth+xi+i+1)*4+1];
+	      blue += yfrag * src[(yi*swidth+xi+i+1)*4+2];
+	      alpha += yfrag * src[(yi*swidth+xi+i+1)*4+3];
+	    } 
+      
+	  red += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4];
+	  green += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4+1];
+	  blue += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4+2];
+	  alpha += xfrag2 * yfrag * src[(yi*swidth+xi+i+1)*4+3];
         
-        red += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4]; 
-        green += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4+1]; 
-        blue += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4+2]; 
-        alpha += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4+3]; 
-      }
-
+      
+	  for(i=0; yi+i+1 < yt +dy-1 && yi + i+1 < sheight;i++)
+	    {
+	      red += xfrag * src[((yi+i+1)*swidth+xi)*4];
+	      green += xfrag * src[((yi+i+1)*swidth+xi)*4+1];
+	      blue += xfrag * src[((yi+i+1)*swidth+xi)*4+2];
+	      alpha += xfrag * src[((yi+i+1)*swidth+xi)*4+3];
+        
+	      for (ii = 0; xi + ii + 1 < xt + dx - 1 && xi + ii + 1 < swidth; ii++)
+		{
+		  red += src[((yi+i+1)*swidth+xi+ii+1)*4];
+		  green += src[((yi+i+1)*swidth+xi+ii+1)*4+1];
+		  blue += src[((yi+i+1)*swidth+xi+ii+1)*4+2];
+		  alpha += src[((yi+i+1)*swidth+xi+ii+1)*4+3];
+		}
+          
+	      if (yi + i + 1 < sheight && xi + ii + 1 < swidth)
+		{
+		  red += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4];
+		  green += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4+1];
+		  blue += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4+2];
+		  alpha += xfrag2 * src[((yi+i+1)*swidth+xi+ii+1)*4+3];
+		}
+	    }
+    
 	  if (yi + i + 1 < sheight)
-	  {
-		  red += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4];
-		  green += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4 + 1];
-		  blue += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4 + 2];
-		  alpha += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4 + 3];
+	    {
+	      red += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4];
+	      green += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4 + 1];
+	      blue += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4 + 2];
+	      alpha += xfrag * yfrag2 * src[((yi + i + 1)*swidth + xi) * 4 + 3];
 
-		  for (ii = 0; xi + ii + 1 < xt + dx - 1 && xi + ii + 1 < swidth; ii++)
-		  {
-			  red += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4];
-			  green += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 1];
-			  blue += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 2];
-			  alpha += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 3];
-		  }
-	  }
- 
-	  if (yi + i + 1 < sheight && x + xi + 1 < swidth)
-	  {
-		  red += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4];
-		  green += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 1];
-		  blue += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 2];
-		  alpha += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 3];
-	  }
-     red /= dx * dy;
-     green /= dx * dy;
-     blue /= dx * dy;
-     alpha /= dx * dy;
-     red = clamp(red, 0, 255);
-     green = clamp(green, 0, 255);
-     blue = clamp(blue, 0, 255);
-     alpha = clamp(alpha, 0, 255);
+	      for (ii = 0; xi + ii + 1 < xt + dx - 1 && xi + ii + 1 < swidth; ii++)
+		{
+		  red += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4];
+		  green += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 1];
+		  blue += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 2];
+		  alpha += yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 3];
+		}
+	    }
+      
+	  if (yi + i + 1 < sheight && xi + ii + 1 < swidth)
+	    {
+	      red += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4];
+	      green += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 1];
+	      blue += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 2];
+	      alpha += xfrag2 * yfrag2 * src[((yi + i + 1)*swidth + xi + ii + 1) * 4 + 3];
+	    }
+       
+        
+	  red /= dx * dy;
+	  green /= dx * dy;
+	  blue /= dx * dy;
+	  alpha /= dx * dy;
+
+	  red = clamp(red, 0, 255);
+	  green = clamp(green, 0, 255);
+	  blue = clamp(blue, 0, 255);
+	  alpha = clamp(alpha, 0, 255);
    
-     dest[(y*dwidth+x)*4] = (unsigned char) red;
-     dest[(y*dwidth+x)*4+1] = (unsigned char) green;
-     dest[(y*dwidth+x)*4+2] = (unsigned char) blue;
-     dest[(y*dwidth+x)*4+3] = (unsigned char) alpha;
+	  dest[(y*dwidth+x)*4] = (unsigned char) red;
+	  dest[(y*dwidth+x)*4+1] = (unsigned char) green;
+	  dest[(y*dwidth+x)*4+2] = (unsigned char) blue;
+	  dest[(y*dwidth+x)*4+3] = (unsigned char) alpha;
+	}
     }
-  }
 
 
 }
